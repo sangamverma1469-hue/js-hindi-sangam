177a4d4895fb8c361d574b16a7a46db7bfbad26c
sangamverma1469-hue <sangamverma1469@gmail.com>
2026-09-07 17:18:33 +0000

    Comparision

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -35,8 +35,8 @@
 
 // *************************** Operations ***************************
 
-let value = 3
-let negValue = -value
+// let value = 3
+// let negValue = -value
 // console.log(negValue);
 
 // console.log(2+2);
@@ -46,10 +46,10 @@ let negValue = -value
 // console.log(2/2);
 // console.log(2%2);
 
-let str1 = "Hello"
-let str2 = " Sangam"
+// let str1 = "Hello"
+// let str2 = " Sangam"
 
-let str3 = str1 + str2
+// let str3 = str1 + str2
 // console.log(str3);
 
 // console.log("1" + 2);
