e00995c8f187a113212178c657750d18ef3a7489
sangamverma1469-hue <sangamverma1469@gmail.com>
2026-09-08 15:36:07 +0000

    Stack & Heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,40 +1,63 @@
 // Primitive
 
+
+
 // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.3
-
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
-
-const id = Symbol('123')
-const anotherId = Symbol('123')
-
-console.log(id === anotherId);
+// 
 
 // const bigNumber = 7643248474537874n
 
+
+
 // Reference (Non primitive)
+
+
 
 // Array, Objects, Functions
 
-const heros = ["Shaktiman", "Naagraj", "Doga"];
-let myObj = {
-    name: "Sangam",
-    age: 20,
+// const heros = ["Shaktiman", "Naagraj", "Doga"];
+// let myObj = {
+//     name: "Sangam",
+//     age: 20,
+// }
+
+// const myFunction = function(){
+//     console.log("Hello World!");
+// }
+
+// console.log(typeof bigNumber);   //> undefined
+// console.log(typeof outsideTemp); //> object
+// console.log(typeof scoreValue); //> number
+// console.log(typeof myFunction); //> function
+// console.log(typeof heros); //> object
+// console.log(typeof anotherId); //> symbol
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// Stack (Primitive), Heap (Non-Primitive) 
+
+let myYoutubename = "sangamverma.com"
+
+let anothername = myYoutubename
+anothername = "chaiurcode"
+
+console.log(myYoutubename);
+
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
 }
 
-const myFunction = function(){
-    console.log("Hello World!");
-}
+let userTwo = userOne
 
-console.log(typeof bigNumber);   //> undefined
-console.log(typeof outsideTemp); //> object
-console.log(typeof scoreValue); //> number
-console.log(typeof myFunction); //> function
-console.log(typeof heros); //> object
-console.log(typeof anotherId); //> symbol
+userTwo.email = "sangam@google.com"
 
+console.log(userOne.email);
+console.log(userTwo.email);
 
